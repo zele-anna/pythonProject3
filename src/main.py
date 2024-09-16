7e1a2ece7a516823a16ea2a096c4a8d95f7966cf
Anna <azeleranskaya@gmail.com>
2024-09-16 15:09:29 +0300

    save data in file

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from typing import List
 import re
 
 def get_names_from_file(file_name :str) -> List[str]:
-    """Функция выбирает имена из файла, игнорируя знаки препинания и числовые значения,
-    и возвращает список имен"""
+    """ Функция выбирает имена из файла, игнорируя знаки препинания и числовые значения,
+    и возвращает список имен. """
     with open(('data/' + file_name), "r", encoding='UTF-8') as file:
         names_list = re.split(r"\n|[0-9]| |,", file.read())
         pure_names_list = []
@@ -18,20 +18,30 @@ def get_names_from_file(file_name :str) -> List[str]:
 
 
 def is_cyrillic(name_item: str) -> bool:
-    """Проверка на вхождение кириллицы в стоку"""
+    """ Проверка на вхождение кириллицы в строку. """
     return bool(re.search('[а-яА-я]', name_item))
 
 
 def filter_russian_names(names_list: str) -> list:
-    """Фильтрация русских имен"""
+    """ Фильтрация русских имен. """
     russian_names_list = []
     for name_item in names_list:
         if is_cyrillic(name_item):
             russian_names_list.append(name_item)
     return russian_names_list
 
-names_list = get_names_from_file('names.txt')
-russian_names = filter_russin_names(names_list)
 
+def save_to_file(file_name: str, data: str) -> None:
+    """ Сохраняет данные в файл. """
+    with open('data/' + file_name, 'w') as names_file:
+        names_file.write(data)
+
+names_list = get_names_from_file('names.txt')
+russian_names = filter_russian_names(names_list)
+
+save_to_file(
+    'russian_names.txt',
+    '\n'.join(russian_names)
+)
 for name in russian_names:
     print(name)

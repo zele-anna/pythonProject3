ff5b9bd289b84695ee4f61a1435086369a261c4f
Anna <azeleranskaya@gmail.com>
2024-09-16 14:43:24 +0300

    add function for getting pure names from file (repeat)

--- src/main.py
+++ src/main.py
@@ -6,15 +6,15 @@ def get_names_from_file(file_name :str) -> List[str]:
     и возвращает список имен"""
     with open(('data/' + file_name), "r", encoding='UTF-8') as file:
         names_list = re.split(r"\n|[0-9]| |,", file.read())
-        cleared_names_list = []
+        pure_names_list = []
         for item in names_list:
-            cleared_name = ""
+            pure_name = ""
             for symbol in item:
                 if symbol.isalpha():
-                    cleared_name += symbol
-            if cleared_name.isalpha():
-                cleared_names_list.append(cleared_name)
-    return cleared_names_list
+                    pure_name += symbol
+            if pure_name.isalpha():
+                pure_names_list.append(cleared_name)
+    return pure_names_list
 
 names_list = get_names_from_file('names.txt')
 for name in names_list:
